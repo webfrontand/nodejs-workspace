0a8166ec744d62e937f55181241cd0daa55b23cb
webfrontand <fbrud431@gmail.com>
2017-03-31 19:56:54 +0900

    start kit

---
+++ starter kit/client/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                hello world
+            </div>
+            // <h1>Hello World from react - hot reload</h1>
+        );
+    }
+}
+
+export default App;
