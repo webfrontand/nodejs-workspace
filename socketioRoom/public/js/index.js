cf84a81290debb5dffbada32971e1ab47c74cc3f
webfrontand <fbrud431@gmail.com>
2017-04-04 14:16:46 +0900

    socket

--- socketioRoom/public/js/index.js
+++ socketioRoom/public/js/index.js
@@ -1,14 +1,41 @@
-var socket = io();
 
-socket.on('connect', function() {
-  console.log('connect to server');
+$(function(){
 
-});
+  var socket = io();
+  var messageForm = $('#message-form');
+  var messageInput = $('#message-input');
+  var lists = $('#lists');
 
-socket.on('disconnect', function() {
-  console.log('disconnect from server');
-});
+  socket.on('connect', function() {
+    console.log('connect to server');
 
-socket.on('newMessage', function(message) {
-  console.log(message);
-});
+  });
+
+  socket.on('disconnect', function() {
+    console.log('disconnect from server');
+  });
+
+  socket.on('newMessage', function(message) {
+    console.log(message);;
+    lists.append(`<li>${message.text}</li>`);
+    messageInput.val('');
+  });
+
+  socket.emit('createMessage', {
+    from: 'Frank',
+    text: 'Hi'
+  }, function(data) {
+    console.log(data);
+  });
+
+  messageForm.on('submit', function(e){
+    e.preventDefault();
+    socket.emit('createMessage', {
+      from: 'User',
+      text: messageInput.val()
+    }, function() {
+
+    });
+  })
+
+}());
